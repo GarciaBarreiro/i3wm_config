c8c422a587a5975d649b540104e900cd526123d6
GarciaBarreiro <tomas.garcia.prof@gmail.com>
2023-09-02 12:59:38 +0200

    changed LG `i3status.sh`, fixed numpad commands

--- i3status.sh
+++ i3status.sh
@@ -3,6 +3,6 @@
 i3status -c ~/.config/i3/i3status.conf | while :
 do
         read line
-        LG=$(setxkbmap -query | awk '/layout/{print $2}') 
+        LG=$(fcitx5-remote -n) 
         echo "LG: $LG | $line" || exit 1
 done
