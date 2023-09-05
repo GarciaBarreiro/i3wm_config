5d9da1664a2fc8eb482432e8c45c90d42d1f2af4
GarciaBarreiro <tomas.garcia.prof@gmail.com>
2023-09-05 21:02:55 +0200

    added VOL to i3status, whatsapp rules, started with screenshots

--- i3status.sh
+++ i3status.sh
@@ -4,5 +4,6 @@ i3status -c ~/.config/i3/i3status.conf | while :
 do
         read line
         LG=$(fcitx5-remote -n) 
-        echo "LG: $LG | $line" || exit 1
+        VOL=$(pactl get-sink-volume @DEFAULT_SINK@ | grep -oP '[0-9]*%' | head -n 1) # don't ask
+        echo "VOL: $VOL | LG: $LG | $line" || exit 1
 done
