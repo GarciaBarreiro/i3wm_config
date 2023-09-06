566cd4b7a038ef595644974c632e239c8528d54b
GarciaBarreiro <tomas.garcia.prof@gmail.com>
2023-09-06 19:31:33 +0200

    added xrandr layout management, updated README

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # My i3wm config
 
-WIP
+This isn't very well put together.
+
+`config` is the i3wm configuration file.
+`config.old` is the original configuration file.
+`i3status.sh` and `i3status.conf` are i3status configuration files (barebones, as of now).
+`layouts.sh` is a simple bash script that reads `~/.screenlayout` and lets me select one of them quickly.
+I'm not sure what `toggletouchpad.sh` is.
---
+++ layouts.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# this isn't the best way to do this
+
+list=$( ls ~/.screenlayout/ )
+
+echo "Layouts:"
+
+i=0
+for layout in $list; do
+    echo -e "  $i\t$layout"
+    let i++
+done
+let i--
+
+read -p "Enter layout num: " num
+
+list=($( ls ~/.screenlayout/ ))
+if [ -n "$num" ] && [ "$num" -eq "$num" ] && [ "$num" -ge 0 ] 2>/dev/null; then
+    exec "~/.screenlayout/${list[$num]}"
+else
+    echo "ERROR"
+fi
+
+exit
